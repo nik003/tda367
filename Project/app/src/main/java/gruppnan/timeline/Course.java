c2abe48ac6dd03116077ffbb86ce1a4534293773
mellis3000 <melinaandersson1@gmail.com>
2017-04-04 10:50:21 +0200

    Added Event and Course

---
+++ Project/app/src/main/java/gruppnan/timeline/Course.java
@@ -0,0 +1,13 @@
+package gruppnan.timeline;
+
+/**
+ * Created by Melina on 04/04/2017.
+ */
+
+public class Course {
+
+    private String name, courseID;
+    //private Timer week, session;
+
+
+}
---
+++ Project/app/src/main/java/gruppnan/timeline/Event.java
@@ -0,0 +1,14 @@
+package gruppnan.timeline;
+
+import java.util.Date;
+
+/**
+ * Created by Melina on 04/04/2017.
+ */
+
+public abstract class Event extends Course {
+
+    private Date endDate;
+    private String description, name;
+    private Course course;
+}
