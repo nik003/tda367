7bad6abe1a13137105ce59147fb19ab1e9b97952
nik003 <nikko.dahlberg@hotmail.com>
2017-05-03 23:09:24 +0200

    Testing viewing the fragment

--- Project/app/src/main/java/gruppnan/timeline/MainActivity.java
+++ Project/app/src/main/java/gruppnan/timeline/MainActivity.java
@@ -1,14 +1,22 @@
 package gruppnan.timeline;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+
+
+
     }
 }
