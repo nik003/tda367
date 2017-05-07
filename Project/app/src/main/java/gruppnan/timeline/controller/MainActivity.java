b3ff9ddc62471a3b7b0efcd7e63d83be3d1ee88d
mellis3000 <melinaandersson1@gmail.com>
2017-05-07 21:16:08 +0200

    nav

--- Project/app/src/main/java/gruppnan/timeline/controller/MainActivity.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/MainActivity.java
@@ -1,27 +1,81 @@
 package gruppnan.timeline.controller;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import gruppnan.timeline.R;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Intent intent = new Intent(this, TimelineActivity.class);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(intent);
+        configureToolbar();
+        initDrawer();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        //actionbar.setHomeAsUpIndicator(R.drawable.);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initDrawer() {
+        /*
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int itemId = menuItem.getItemId();
+
+                if (itemId == R.id.timeline) {
+                    //setContentView(R.id.timelineView);
+                    mDrawerLayout.closeDrawers();
+                } else if (itemId == R.id.timer) {
+                    //setContentView(R.id.timerView);
+                    mDrawerLayout.closeDrawers();
+                }
+
+                return false;
+
             }
-    });
-}
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch(itemId) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            //case
+        }
+
+        return true;
+        */
+    }
+
 }
