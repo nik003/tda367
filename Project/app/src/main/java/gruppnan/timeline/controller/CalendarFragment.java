e96b72c9bb33ef3665c8270320ab0c5d048ad324
Hannes Thell <hannes.thell@live.se>
2017-05-12 12:07:19 +0100

    updated list in calender view

--- Project/app/src/main/java/gruppnan/timeline/controller/CalendarFragment.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/CalendarFragment.java
@@ -2,23 +2,23 @@ package gruppnan.timeline.controller;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.View;
-import gruppnan.timeline.R;
-import gruppnan.timeline.controller.AddEventFragment;
-
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ListView;
+import android.R.layout.*;
 
 import com.github.clans.fab.FloatingActionButton;
 
+import java.util.ArrayList;
+
 import gruppnan.timeline.R;
+
 
 
 public class CalendarFragment extends Fragment {
@@ -27,6 +27,7 @@ public class CalendarFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private FloatingActionButton fab1,fab2;
+    private Button weekViewButton;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -34,6 +35,7 @@ public class CalendarFragment extends Fragment {
     private FragmentManager fragmentManager;
     private FragmentTransaction ft;
     private CalendarView calendarView;
+    private ListView eventListView;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -74,8 +76,27 @@ public class CalendarFragment extends Fragment {
         fab1.setOnClickListener(btnListener);
         fab2.setOnClickListener(btnListener);
 
+        eventListView = (ListView) v.findViewById(R.id.eventListView);
+        setUpListView();
+
+        weekViewButton = (Button) v.findViewById(R.id.weekViewBtn);
+        weekViewButton.setOnClickListener(btnListener);
+
         calendarView = (CalendarView) v.findViewById(R.id.calendarView);
 
+    }
+
+    private void setUpListView(){
+        //String values [] = new String[]{"hej", "okej", "nej"};
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("this");
+        lista.add("is");
+        lista.add("a");
+        lista.add("list");
+        lista.add("does it scroll?");
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,lista );
+        eventListView.setAdapter(adapter);
     }
 
     private View.OnClickListener btnListener = new View.OnClickListener() {
@@ -95,9 +116,6 @@ public class CalendarFragment extends Fragment {
                 fragmentManager = getActivity().getSupportFragmentManager();
                 ft = fragmentManager.beginTransaction();
                 ft.replace(R.id.calendar_fragment, newFragment).addToBackStack(null).commit();
-
-
-
             }
             else if (view.equals(fab2)){
                 bundle.putString("type", "deadline");
@@ -109,6 +127,12 @@ public class CalendarFragment extends Fragment {
                 ft = fragmentManager.beginTransaction();
                 ft.replace(R.id.calendar_fragment, newFragment).addToBackStack(null).commit();
 
+            }
+            else if (view.equals(weekViewButton)){
+                Fragment weekViewFragment = new WeekViewController();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.calendar_fragment, weekViewFragment).addToBackStack(null).commit();
             }
         }
     };
