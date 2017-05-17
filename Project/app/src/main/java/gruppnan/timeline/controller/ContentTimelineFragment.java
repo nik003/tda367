996e246d38395da7a4b3b7a15282a672d8f48da0
mellis3000 <melinaandersson1@gmail.com>
2017-05-17 12:05:48 +0200

    weekly goal time setting

--- Project/app/src/main/java/gruppnan/timeline/controller/ContentTimelineFragment.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/ContentTimelineFragment.java
@@ -2,57 +2,27 @@ package gruppnan.timeline.controller;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import gruppnan.timeline.ItemListAdapter;
-import gruppnan.timeline.R;
-import gruppnan.timeline.model.DeadlineEventSet;
 import gruppnan.timeline.view.ContentTimelineView;
-import gruppnan.timeline.view.EventSorter;
 
 public class ContentTimelineFragment extends Fragment {
 
     ContentTimelineView mConentTimelineView;
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-
-    private List<DeadlineEventSet> sortedEventSet = new ArrayList<>();
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         mConentTimelineView = new ContentTimelineView(inflater,container);
-        mLayoutManager = new LinearLayoutManager(inflater.getContext());
-        initList();
+
+        //Update view
+        mConentTimelineView.getAdapter().notifyDataSetChanged();
+
         return mConentTimelineView.getRootView();
     }
-
-    public void initList(){
-
-        mRecyclerView = (RecyclerView) mConentTimelineView.getRootView().findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-
-        EventSorter eventSorter = new EventSorter();
-        sortedEventSet = eventSorter.getSortedEventList();
-        mAdapter = new ItemListAdapter(sortedEventSet);
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-
-        mAdapter.notifyDataSetChanged();
-
-
-    }
-
 
 
     @Override
--- Project/app/src/main/java/gruppnan/timeline/controller/SettingsFragment.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/SettingsFragment.java
@@ -1,12 +1,17 @@
 package gruppnan.timeline.controller;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import gruppnan.timeline.R;
 
@@ -18,9 +23,11 @@ public class SettingsFragment extends Fragment {
 
     private SearchView searchView;
     private Spinner courseSpinner;
+    NumberPicker numberPicker;
+    TextView timeText;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_layout,
                 container, false);
@@ -29,7 +36,54 @@ public class SettingsFragment extends Fragment {
         courseSpinner = (Spinner) view.findViewById(R.id.settings_course_spinner);
         courseSpinner.setPrompt("Choose course");
 
+        timeText = (TextView)view.findViewById(R.id.time_picker_text);
+
+        timeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimeDialog(inflater);
+            }
+        });
+
         return view;
+    }
+
+    public void showTimeDialog(final LayoutInflater inflater){
+        View npView = inflater.inflate(R.layout.time_picker_dialog, null);
+
+        numberPicker = (NumberPicker) npView.findViewById(R.id.number_picker);
+        String[] data = new String[]{"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(data.length-1);
+        numberPicker.setValue(15);
+        numberPicker.setDisplayedValues(data);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(inflater.getContext());
+        alertDialogBuilder.setTitle("Select number of hours");
+        alertDialogBuilder.setView(npView);
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                String s = "" + numberPicker.getValue();
+                                timeText.setText(s);
+                                Log.e("","New Quantity Value : "+ numberPicker.getValue());
+
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+
     }
 
     @Override
