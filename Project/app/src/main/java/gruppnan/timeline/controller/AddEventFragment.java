94570c8aff1823f63f53e8f3a9fa642f770df6fc
Hannes Thell <hannes.thell@live.se>
2017-05-17 18:23:02 +0100

    can now add deadlines and default events

--- Project/app/src/main/java/gruppnan/timeline/controller/AddEventFragment.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/AddEventFragment.java
@@ -47,7 +47,8 @@ public class AddEventFragment extends Fragment implements TimePickerDialog.OnTim
     private Spinner courseSpinner;
     private Button whichButton;
     private String eventName, eventDescription, showEndTime, showStarTime;
-    private Course course;
+    //TODO fix course when possible.
+    private Course course = new Course("course1", null);
     private long dateL;
     private int year, month,day;
     private Date yearMonthDay, completeStartDate, completeEndDate;
@@ -67,21 +68,7 @@ public class AddEventFragment extends Fragment implements TimePickerDialog.OnTim
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @return A new instance of fragment addEventFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddEventFragment newInstance(String param1) {
-        AddEventFragment fragment = new AddEventFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
