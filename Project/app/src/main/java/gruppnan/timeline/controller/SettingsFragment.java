8f52cd32ccbfe6c6c3ce62df2d563f42af137a75
mellis3000 <melinaandersson1@gmail.com>
2017-05-19 15:51:57 +0200

    spinner in settings

--- Project/app/src/main/java/gruppnan/timeline/controller/SettingsFragment.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/SettingsFragment.java
@@ -19,8 +19,11 @@ import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.HashSet;
+
 import gruppnan.timeline.R;
 import gruppnan.timeline.model.Course;
+import gruppnan.timeline.model.CourseContainer;
 
 /**
  * Created by Melina on 13/05/2017.
@@ -34,7 +37,10 @@ public class SettingsFragment extends Fragment {
     TextView timeText;
     LinearLayout settingsLayout;
     ArrayAdapter<String> adapter;
-    Course course;
+    String selectedCourse;
+    HashSet<Course> allCourses = new HashSet<>();
+    String coursesInSpinner[];
+    int currValue;
 
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
@@ -47,9 +53,61 @@ public class SettingsFragment extends Fragment {
         courseSpinner = (Spinner) view.findViewById(R.id.settings_course_spinner);
         timeText = (TextView)view.findViewById(R.id.time_picker_text);
 
-        courseSpinner.setPrompt("Choose course");
-        searchView.setQueryHint("Search course (TDA367)");
+        initCourseSpinner();
+        initSearchView(inflater);
+        setListeners(inflater);
 
+        return view;
+    }
+
+    public void setListeners(final LayoutInflater inflater){
+        settingsLayout.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent ev)
+            {
+                //hideKeyboard();
+                return false;
+            }
+        });
+
+
+        timeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimeDialog(inflater);
+            }
+        });
+
+        courseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedCourse = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Do nothing
+            }
+        });
+    }
+
+    public void initCourseSpinner(){
+        courseSpinner.setPrompt("Choose course");
+        coursesInSpinner = new String[2];
+        int i=0;
+        for(Course c : CourseContainer.getCourseContainer().getAllCourses()) {
+            allCourses.add(c);
+            coursesInSpinner[i] = c.getCourseID();
+            i++;
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, coursesInSpinner);
+        courseSpinner.setAdapter(adapter);
+    }
+
+
+    public void initSearchView(final LayoutInflater inflater){
+        searchView.setQueryHint("Search course (TDA367)");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
 
             @Override
@@ -63,32 +121,6 @@ public class SettingsFragment extends Fragment {
                 return false;
             }
         });
-
-        setListeners(inflater);
-
-        return view;
-    }
-
-    public void setListeners(final LayoutInflater inflater){
-        settingsLayout.setOnTouchListener(new View.OnTouchListener()
-        {
-            @Override
-            public boolean onTouch(View view, MotionEvent ev)
-            {
-                hideKeyboard();
-                //searchView.showDropDown();
-                return false;
-            }
-        });
-
-
-        timeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showTimeDialog(inflater);
-            }
-        });
-
     }
 
     public void showTimeDialog(final LayoutInflater inflater){
@@ -99,7 +131,7 @@ public class SettingsFragment extends Fragment {
         String[] data = new String[]{"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
         numberPicker.setMinValue(0);
         numberPicker.setMaxValue(data.length-1);
-        numberPicker.setValue(15);
+        numberPicker.setValue(currValue);
         numberPicker.setDisplayedValues(data);
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
@@ -114,8 +146,8 @@ public class SettingsFragment extends Fragment {
                             public void onClick(DialogInterface dialog,
                                                 int id) {
                                 String s = "" + numberPicker.getValue();
-                                //course.setWeeklyGoal(numberPicker.getValue());
                                 timeText.setText(s);
+                                setGoal(numberPicker.getValue());
 
                             }
                         })
@@ -136,9 +168,9 @@ public class SettingsFragment extends Fragment {
         View cView = inflater.inflate(R.layout.course_result_dialog, null);
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
-        String courses[] = {"hej","dä","hej","dä","hej","dä","hej","dä","hej","dä","hej","dä","hej","dä","hej","dä"};
+        String searchMatches[] = {"hej","dä","hej","dä","hej","dä","hej","dä","hej","dä","hej","dä","hej","dä","hej","dä"};
         final ListView listView = (ListView) cView.findViewById(R.id.course_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(),android.R.layout.simple_list_item_1,courses);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(),android.R.layout.simple_list_item_1,searchMatches);
         listView.setAdapter(adapter);
 
 
@@ -176,8 +208,14 @@ public class SettingsFragment extends Fragment {
         });
     }
 
-
-
+    public void setGoal(int value){
+        for(Course c : allCourses){
+            if(c.getCourseID() == selectedCourse){
+                c.setWeeklyGoal(value);
+                // currValue = c.getWeeklyGoal(); ändra
+            }
+        }
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
