721e2ca3eedae9ea5011fbcbd4d06b62641eb742
Hannes Thell <hannes.thell@live.se>
2017-05-25 15:00:17 +0100

    refactored listener implementation

--- Project/app/src/main/java/gruppnan/timeline/controller/EventAdapter.java
+++ Project/app/src/main/java/gruppnan/timeline/controller/EventAdapter.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
@@ -37,17 +38,13 @@ import gruppnan.timeline.model.EventInterface;
 public class EventAdapter extends ArrayAdapter<Event> {
 
     private int layoutId;
-    private FloatingActionButton editEventBtn;
-    private FragmentManager fragmentManager;
-    private Fragment fragment;
-    private Calendar calendar;
-    private int year,month,day;
+    private FloatingActionButton editEventBtn, deleteEventBtn;
 
 
-    public EventAdapter(@NonNull Context context, int layoutResourceId, ArrayList<Event> events, Fragment fragment) {
+
+    public EventAdapter(@NonNull Context context, int layoutResourceId, ArrayList<Event> events) {
         super(context, layoutResourceId, events);
         this.layoutId = layoutResourceId;
-        this.fragment = fragment;
 
     }
 
@@ -57,59 +54,17 @@ public class EventAdapter extends ArrayAdapter<Event> {
 
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(this.getContext()).inflate(layoutId, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(layoutId, parent, false);
         }
 
         final EventInterface eTmp = getItem(position);
         TextView eventName = (TextView) convertView.findViewById(R.id.eventNameTxt1);
         TextView eventType = (TextView) convertView.findViewById(R.id.typeTxt);
         editEventBtn = (FloatingActionButton) convertView.findViewById(R.id.editEventBtn);
-        editEventBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle bundle = new Bundle();
+        deleteEventBtn = (FloatingActionButton) convertView.findViewById(R.id.deleteEventBtn);
 
-                Fragment newFragment = new AddEventFragment();
-                bundle.putString("name", eTmp.getName());
-                bundle.putString("description", eTmp.getDescription());
-                Date endDate = eTmp.getEndDate();
-                Date startDate = eTmp.getStartDate();
-                calendar = Calendar.getInstance();
-                setDate(startDate, endDate, bundle, eTmp);
-                calendar.setTime(endDate);
-                year = calendar.get(Calendar.YEAR);
-                month = calendar.get(Calendar.MONTH);
-                day = calendar.get(Calendar.DAY_OF_MONTH);
-                int endHour = calendar.get(Calendar.HOUR_OF_DAY);
-                int endMinute = calendar.get(Calendar.MINUTE);
-                bundle.putInt("year", year);
-                bundle.putInt("month", month);
-                bundle.putInt("day", day);
-                bundle.putInt("endHour", endHour);
-                bundle.putInt("endMinute", endMinute);
-                bundle.putInt("id", eTmp.getID());
-
-                if (eTmp instanceof DefaultEvent){
-                    calendar.setTime(startDate);
-
-                    int startHour = calendar.get(Calendar.HOUR_OF_DAY);
-                    int startMinute = calendar.get(Calendar.MINUTE);
-                    bundle.putString("type", "event");
-                    bundle.putInt("startHour", startHour);
-                    bundle.putInt("startMinute", startMinute);
-                }
-
-                if (eTmp instanceof DeadlineEvent){
-                    bundle.putString("type", "deadline");
-                }
-                newFragment.setArguments(bundle);
-
-
-                fragmentManager = fragment.getActivity().getSupportFragmentManager();
-                FragmentTransaction ft = fragmentManager.beginTransaction();
-                ft.replace(R.id.calendar_fragment, newFragment).addToBackStack(null).commit();
-            }
-        });
+        editEventBtn.setOnClickListener(new EventListener(getContext(),eTmp));
+        deleteEventBtn.setOnClickListener(new EventListener(getContext(),eTmp));
 
         eventName.setText("Name: " + eTmp.getName() );
 
@@ -122,8 +77,6 @@ public class EventAdapter extends ArrayAdapter<Event> {
 
         return convertView;
     }
-    private void setDate(Date startDate, Date endDate, Bundle bundle, EventInterface eTmp){
-        //was here
-    }
+
 
 }
